24e5be5a06674dd5410a1d8e2afaa5e46e42caf5
lktv <tanturv@gmail.com>
2025-03-20 01:42:38 +0700

    Initial commit for Heroku

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
-import mysql from 'mysql2/promise.js';
+import mysql from 'mysql2/promise';
 
-// Tạo nhóm kết nối đến cơ sở dữ liệu MySQL
+// Lấy thông tin kết nối từ biến môi trường (Heroku)
 const pool = mysql.createPool({
-  host: 'localhost', // Thay bằng host của bạn
-  user: 'root',      // Thay bằng user của bạn
-  password: '0000',  // Thay bằng mật khẩu của bạn
-  database: 'chemicals' // Thay bằng tên database của bạn
+  host: process.env.DB_HOST || '192.168.1.3', // IP hoặc domain của MySQL server
+  user: process.env.DB_USER || 'heroku_user',   // User của MySQL server
+  password: process.env.DB_PASSWORD || '0000', // Mật khẩu
+  database: process.env.DB_NAME || 'chemicals',     // Tên database
+  port: process.env.DB_PORT || 3306, // Port mặc định của MySQL là 3306
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// Export nhóm kết nối để sử dụng ở các module khác
+
 export default pool;
+
+
